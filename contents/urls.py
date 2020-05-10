b9f2f6a3efb820927c0ac6efc873579793efa3d8
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-10 18:14:56 -0300

    Updated views emporio package to class-based form

--- contents/urls.py
+++ contents/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from socialize.socialize.urls import accounts_patterns
 from shipping.shipping.urls import deliveries_patterns
 from plethora.plethora.urls import contents_patterns
-from emporio.emporio.urls import products_patterns, baskets_patterns
+from emporio.emporio.urls import products_patterns, baskets_patterns, orders_patterns
 from feedly.feedly.urls import blocks_patterns
 
 # from django.contrib.auth.models import User
@@ -37,6 +37,7 @@ urlpatterns = [
     path('contents/', include(contents_patterns)),
     path('products/', include(products_patterns)),
     path('baskets/', include(baskets_patterns)),
+    path('orders/', include(orders_patterns)),
     path('blocks/', include(blocks_patterns)),
     # path('users/', UsersView.as_view()),
 ]
