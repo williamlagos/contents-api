9c3a680fedf5608b486b601827aadf98c5b0eeea
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-11 23:18:26 -0300

    Updated first urls on package

--- contents/urls.py
+++ contents/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views import View
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from socialize.socialize.urls import accounts_patterns
 from shipping.shipping.urls import deliveries_patterns
@@ -21,16 +23,12 @@ from plethora.plethora.urls import contents_patterns
 from emporio.emporio.urls import products_patterns, baskets_patterns, orders_patterns
 from feedly.feedly.urls import blocks_patterns
 
-# from django.contrib.auth.models import User
-# from django.views import View
-# from django.http import JsonResponse
-# from django.core import serializers
-
-# class UsersView(View):
-#     def get(self, request):
-#         return JsonResponse(User.objects.all().values()[0])
+class HealthCheckView(View):
+    def get(self, request):
+        return JsonResponse({'health': 'success'})
 
 urlpatterns = [
+    path('', HealthCheckView.as_view()),
     path('dashboard/', admin.site.urls),
     path('deliveries/', include(deliveries_patterns)),
     path('accounts/', include(accounts_patterns)),
@@ -39,5 +37,4 @@ urlpatterns = [
     path('baskets/', include(baskets_patterns)),
     path('orders/', include(orders_patterns)),
     path('blocks/', include(blocks_patterns)),
-    # path('users/', UsersView.as_view()),
 ]
