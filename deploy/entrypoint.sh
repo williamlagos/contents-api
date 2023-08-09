27350a4e30c610b268d423d67ca64b890b6184c6
William Oliveira de Lagos <william.lagos@icloud.com>
2023-08-09 18:39:14 +0200

    chore: removed old deployment files for linux and heroku

--- deploy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python manage.py collectstatic --noinput 
-# Verify if the database exists on Postgres
-if !(psql -U postgres -h db -lqt | cut -d \| -f 1 | grep -qw efforia); then
-	psql -U postgres -h db -c "create database efforia;" 
-fi
-python manage.py makemigrations --settings="store.production"
-python manage.py migrate --run-syncdb --settings="store.production" 
-gunicorn store.wsgi -b 0.0.0.0:9000
