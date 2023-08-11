c5116724f13913c35e88c749263e1adb6acb41dd
William Oliveira de Lagos <william.lagos@icloud.com>
2023-08-11 18:29:10 +0200

    feat: added some basic create, read, update and delete methods

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from odmantic import Model
-from fastapi import FastAPI
+from odmantic import AIOEngine, Model, ObjectId
+from fastapi import FastAPI, HTTPException
+from typing import List
 
-app = FastAPI()
-
-class Page(Model):
+class Content(Model):
     title: str
     content: str
 
+app = FastAPI()
+
+engine = AIOEngine()
+
 @app.get("/")
 async def root():
-    return { "message": "Hello World" }
+    return { "health": "OK" }
+
+@app.put("/contents/", response_model=Content)
+async def create_content(content: Content):
+    await engine.save(content)
+    return content
+
+
+@app.get("/contents/", response_model=List[Content])
+async def get_contents():
+    contents = await engine.find(Content)
+    return contents
+
+
+@app.get("/contents/count", response_model=int)
+async def count_contents():
+    count = await engine.count(Content)
+    return count
+
+
+@app.get("/contents/{id}", response_model=Content)
+async def get_content_by_id(id: ObjectId):
+    content = await engine.find_one(Content, Content.id == id)
+    if content is None:
+        raise HTTPException(404)
+    return content
